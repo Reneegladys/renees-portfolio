afecaa31957519bc1595a42fbcb55138b9162f8b
Renée Gladys <renee16@live.se>
2025-10-14 11:07:55 +0200

    removed envelope, favicon

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './footer.css';
+import { FaLinkedin, FaGithub, } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="footer-top">
+                <div className="footer-top-left">
+                    <p>I’m a frontend developer passionate about crafting seamless and engaging user interfaces.
+                        Aspiring to become a full-stack developer, I love exploring both frontend and backend technologies to turn ideas into functional and meaningful web applications.
+                        Every project is an opportunity to grow, learn, and create digital experiences that leave an impact.</p>
+                </div>
+                <div className="footer-top-right">
+                    <a href="https://www.linkedin.com/in/reneegladys">
+                        <FaLinkedin className="social-icon linkedin" />
+                    </a>
+                    <a href="https://github.com/reneegladys">
+                        <FaGithub className="social-icon github" />
+                    </a>
+                </div>
+            </div>
+            <hr />
+            <div className="footer-bottom">
+                <p className="footer-bottom-left">© 2025 Renée Gladys. All rights reserved</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
